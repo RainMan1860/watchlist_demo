c227338035e712bee8892f702e2a82b7c11a25eb
xiuyu <xiuyu@ahpu.edu.cn>
2020-04-15 08:54:31 +0800

    Add index pages

--- app.py
+++ app.py
@@ -11,9 +11,6 @@ def user_page(name):
 	#print('User name is %s' % name)
 	return 'User name is %s' % name +'<h1>hello again</h1> <img src ="http://watchlist.helloflask.com/static/images/totoro.gif" > '
 
-@app.route("/")
-def hello():
-	return  "hello"
 
 @app.route('/test')
 def test_url_for():
@@ -21,3 +18,23 @@ def test_url_for():
 	print(url_for("user_page",name='xy'))
 	print(url_for('test_url_for'))
 	return 'Test page'
+
+name = "Grey Li"
+movies = [
+	{"title":"My Neighbor Totoro","year":"1988"},
+	{"title":"Dead Poets Society","year":"1989"},
+	{"title":"A Perfect World","year":"1993"},
+	{"title":"Leon","year":"1994"},
+	{"title":"Mahjong","year":"1996"},
+	{"title":"Swallowtail Butterfly","year":"1996"},
+	{"title":"King of comedy","year":"1999"},
+	{"title":"Devils on the Doorstep","year":"1999"},
+	{"title":"Wall-E","year":"2008"},
+	{"title":"The pork of Music","year":"2012"},
+]
+
+from flask import render_template
+
+@app.route ("/")
+def index():
+	return render_template("index.html",name=name,movies = movies)
